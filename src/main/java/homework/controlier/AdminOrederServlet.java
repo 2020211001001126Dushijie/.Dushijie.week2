5f178636de3d9bc60a38e3c09e23fb775e8c50d1
2020211001001126Dushijie <Q113268@outlook.com>
2022-05-31 13:50:25 +0800

    week15 commit

---
+++ src/main/java/homework/controlier/AdminOrederServlet.java
@@ -0,0 +1,40 @@
+package homework.controlier;
+
+import homework.dao.OrderDao;
+import homework.modle.Order;
+import homework.modle.Payment;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+
+@WebServlet(name = "AdminOrederServlet", value = "/admin/orderList")
+public class AdminOrederServlet extends HttpServlet {
+    private Connection con=null;
+    public void init() throws ServletException{
+        con=(Connection) getServletContext().getAttribute("con");
+    }
+    public void destroy(){
+        super.destroy();
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Payment> paymentTypeList=Payment.findAllPayment(con);
+        request.setAttribute("paymentTypeList",paymentTypeList);
+        OrderDao orderDao=new OrderDao();
+        List<Order>orderList=orderDao.findAll(con);
+        request.setAttribute("orderList",orderList);
+        String path="/WEB-INF/views/admin/orderList.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+}
